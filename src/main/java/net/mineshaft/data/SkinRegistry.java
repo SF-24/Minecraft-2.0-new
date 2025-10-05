a21c45b97e14aac022fac04468aa71c6532a4c48
SF-24 <legonexoknights123@gmail.com>
2025-10-05 21:03:08 +0100

    Fixes

--- src/main/java/net/mineshaft/data/SkinRegistry.java
+++ src/main/java/net/mineshaft/data/SkinRegistry.java
@@ -1,5 +1,6 @@
 package net.mineshaft.data;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.mojang.authlib.GameProfile;
@@ -8,10 +9,16 @@ import com.mojang.util.UUIDTypeAdapter;
 import net.mineshaft.MineshaftLogger;
 
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -20,14 +27,30 @@ public class SkinRegistry {
 
     protected static JsonParser jsonParser = new JsonParser();
 
+    protected static TrustManager[] trustAllCerts = new TrustManager[] {
+            new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() { return null; }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) { }
+                public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+            }
+    };
+
+
     public static JsonElement parseJson(String json) {
 //        System.out.println("parsing json " + json);
+        if(json==null) return jsonParser.parse("{}");
         return jsonParser.parse(Objects.requireNonNull(json));
     }
 
     public static String getRequest(String url) {
         try {
+            // Set cert acceptation
+//            SSLContext sc = SSLContext.getInstance("SSL");
+//            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+//            connection.setSSLSocketFactory(sc.getSocketFactory());
+
             HttpsURLConnection connection = (HttpsURLConnection) new URL(String.format(url)).openConnection();
+
             if (connection.getResponseCode() == HttpsURLConnection.HTTP_OK) {
                 return new BufferedReader(new InputStreamReader(connection.getInputStream())).lines().collect(Collectors.joining());
             } else {
@@ -36,7 +59,10 @@ public class SkinRegistry {
             }
         } catch (IOException e) {
             MineshaftLogger.logWarning("request for url " + url + " failed: " + e.getMessage());
-            return null;
+            return "{}";
+        } catch (NullPointerException ignored) {
+            MineshaftLogger.logWarning("request for url " + url + " failed: " + "null");
+            return "{}";
         }
     }
 
