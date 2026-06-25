cf8d8119ad8b212e1114fa6be5f0df1331e8218b
SF-24 <legonexoknights123@gmail.com>
2026-06-25 16:57:27 +0100

    Reapply "Merged client and server."
    This reverts commit b423945994cf304b01ecd363cc4d80ee060e8c5b.

--- src/main/java/net/minecraft/network/rcon/RConConsoleSource.java
+++ src/main/java/net/minecraft/network/rcon/RConConsoleSource.java
@@ -18,6 +18,27 @@ public class RConConsoleSource implements ICommandSender
     /** RCon string buffer for log. */
     private StringBuffer buffer = new StringBuffer();
 
+    public static RConConsoleSource getInstance()
+    {
+        return instance;
+    }
+
+    /**
+     * Clears the RCon log
+     */
+    public void resetLog()
+    {
+        this.buffer.setLength(0);
+    }
+
+    /**
+     * Gets the contents of the RCon log
+     */
+    public String getLogContents()
+    {
+        return this.buffer.toString();
+    }
+
     /**
      * Get the name of this object. For players this returns their username
      */
