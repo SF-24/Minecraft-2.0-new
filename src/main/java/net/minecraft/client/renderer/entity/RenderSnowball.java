1ee1f6338fa280edfce054349348e50ebfb01724
SF-24 <legonexoknights123@gmail.com>
2026-06-27 21:44:39 +0100

    Finished the bubble mechanics and rendering.

--- src/main/java/net/minecraft/client/renderer/entity/RenderSnowball.java
+++ src/main/java/net/minecraft/client/renderer/entity/RenderSnowball.java
@@ -1,5 +1,6 @@
 package net.minecraft.client.renderer.entity;
 
+import net.minecraft.MineshaftLogger;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.texture.TextureMap;
@@ -9,6 +10,7 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 public class RenderSnowball<T extends Entity> extends Render<T>
 {
@@ -30,21 +32,34 @@ public class RenderSnowball<T extends Entity> extends Render<T>
         GlStateManager.pushMatrix();
         GlStateManager.translate((float)x, (float)y, (float)z);
         GlStateManager.enableRescaleNormal();
+        GlStateManager.depthMask(false);
+
 
         // Scale the model based on whether it's riding an entity
         if(entity instanceof EntitySnowball && ((EntitySnowball) entity).getProjectileType()==10) {
+
             if (entity.riddenByEntity!=null) {
-                GlStateManager.scale(1.5f,1.5f,1.5f);
+                if(((EntitySnowball) entity).getRenderOffset() == 0f) {
+                    // Cache the values.
+                    ((EntitySnowball) entity).setRenderScale(1.125f * Math.max(entity.riddenByEntity.height, entity.riddenByEntity.width * 1.2f));
+                    ((EntitySnowball) entity).setRenderOffset(entity.riddenByEntity.height / 2.0f);
+                }
+                float renderScale = ((EntitySnowball) entity).getRenderScale();
+                GlStateManager.translate(0, ((EntitySnowball) entity).getRenderOffset(), 0);
+                GlStateManager.scale(renderScale, renderScale, renderScale);
             } else {
-                GlStateManager.scale(0.5f,0.5f,0.5f);
+                GlStateManager.scale(1.0f,1.0f,1.0f);
             }
         } else {
             GlStateManager.scale(0.5F, 0.5F, 0.5F);
         }
         GlStateManager.rotate(-this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
         GlStateManager.rotate(this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
+//        if(entity.riddenByEntity==null||entity.riddenByEntity.isDead) {
+//        }
         this.bindTexture(TextureMap.locationBlocksTexture);
         this.field_177083_e.renderItem(this.getITemStack(entity), ItemCameraTransforms.TransformType.GROUND);
+        GlStateManager.depthMask(true);
         GlStateManager.disableRescaleNormal();
         GlStateManager.popMatrix();
         super.doRender(entity, x, y, z, entityYaw, partialTicks);
