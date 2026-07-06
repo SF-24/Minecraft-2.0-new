5c43a630b5e1f813c3684c36f3111dca131b5c59
SF-24 <legonexoknights123@gmail.com>
2026-07-06 12:27:32 +0100

    Updated loot and made notch apples found only in loot chests (currently in dungeons)

--- src/main/java/net/mineshaft/structure/LootTableList.java
+++ src/main/java/net/mineshaft/structure/LootTableList.java
@@ -1,7 +1,9 @@
 package net.mineshaft.structure;
 
 import com.google.common.collect.Lists;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.util.WeightedRandomChestContent;
 
 import java.util.List;
@@ -9,6 +11,9 @@ import java.util.List;
 public class LootTableList {
 
     public static class LootOverworld {
+        // Rarely has notch apples
+        public static final List<WeightedRandomChestContent> DUNGEON = Lists.newArrayList(new WeightedRandomChestContent(Items.saddle, 0, 1, 1, 10), new WeightedRandomChestContent(Items.iron_ingot, 0, 1, 4, 10), new WeightedRandomChestContent(Items.bread, 0, 1, 1, 10), new WeightedRandomChestContent(Items.wheat, 0, 1, 4, 10), new WeightedRandomChestContent(Items.gunpowder, 0, 1, 4, 10), new WeightedRandomChestContent(Items.string, 0, 1, 4, 10), new WeightedRandomChestContent(Items.bucket, 0, 1, 1, 10), new WeightedRandomChestContent(Items.golden_apple, 1, 1, 1, 1), new WeightedRandomChestContent(Items.redstone, 0, 1, 4, 10), new WeightedRandomChestContent(Items.record_13, 0, 1, 1, 4), new WeightedRandomChestContent(Items.record_cat, 0, 1, 1, 4), new WeightedRandomChestContent(Items.name_tag, 0, 1, 1, 10), new WeightedRandomChestContent(Items.golden_horse_armor, 0, 1, 1, 2), new WeightedRandomChestContent(Items.iron_horse_armor, 0, 1, 1, 5), new WeightedRandomChestContent(Items.diamond_horse_armor, 0, 1, 1, 1));
+
         public static final List<WeightedRandomChestContent> IGLOO = Lists.newArrayList(
                 new WeightedRandomChestContent(Items.golden_apple, 0, 1, 1, 0),
                 new WeightedRandomChestContent(Items.coal, 0, 1, 4, 5),
@@ -20,12 +25,14 @@ public class LootTableList {
         );
 
         public static final List<WeightedRandomChestContent> JUNGLE_PYRAMID = Lists.newArrayList(
-                new WeightedRandomChestContent(Items.diamond, 0, 1, 3, 3),
-                new WeightedRandomChestContent(Items.iron_ingot, 0, 1, 5, 10),
-                new WeightedRandomChestContent(Items.gold_ingot, 0, 2, 7, 15),
+                new WeightedRandomChestContent(Items.diamond, 0, 1, 3, 1), // Rarer, was 3
+                new WeightedRandomChestContent(Items.iron_ingot, 0, 1, 5, 5), // Rarer was 10
+                new WeightedRandomChestContent(Items.gold_ingot, 0, 2, 7, 12), // Rarer was 15
                 new WeightedRandomChestContent(Items.emerald, 0, 1, 3, 2),
-                new WeightedRandomChestContent(Items.bone, 0, 4, 6, 20),
-                new WeightedRandomChestContent(Items.rotten_flesh, 0, 3, 7, 16),
+                new WeightedRandomChestContent(Items.bone, 0, 4, 6, 16), // Rarer
+                new WeightedRandomChestContent(Items.rotten_flesh, 0, 3, 7, 12), // Rarer
+                new WeightedRandomChestContent(Items.string, 0, 3, 10, 5), // New
+                new WeightedRandomChestContent(Items.leather, 0, 3, 10, 5), // New
                 new WeightedRandomChestContent(Items.saddle, 0, 1, 1, 3),
                 new WeightedRandomChestContent(Items.iron_horse_armor, 0, 1, 1, 1),
                 new WeightedRandomChestContent(Items.golden_horse_armor, 0, 1, 1, 1),
@@ -34,12 +41,15 @@ public class LootTableList {
 
 
         public static final List<WeightedRandomChestContent> DESERT_PYRAMID = Lists.newArrayList(
-                new WeightedRandomChestContent(Items.diamond, 0, 1, 3, 3),
-                new WeightedRandomChestContent(Items.iron_ingot, 0, 1, 5, 10),
-                new WeightedRandomChestContent(Items.gold_ingot, 0, 2, 7, 15),
+                new WeightedRandomChestContent(Items.diamond, 0, 1, 3, 1), // Rarer, was 3
+                new WeightedRandomChestContent(Items.iron_ingot, 0, 1, 5, 5), // Rarer, was 10
+                new WeightedRandomChestContent(Items.gold_ingot, 0, 2, 7, 12), // Rarer, was 15
+                new WeightedRandomChestContent(Items.golden_apple, 0, 1, 1, 1),
                 new WeightedRandomChestContent(Items.emerald, 0, 1, 3, 2),
-                new WeightedRandomChestContent(Items.bone, 0, 4, 6, 20),
-                new WeightedRandomChestContent(Items.rotten_flesh, 0, 3, 7, 16),
+                new WeightedRandomChestContent(Items.bone, 0, 4, 6, 16), // Slightly rarer, was 20
+                new WeightedRandomChestContent(Items.rotten_flesh, 0, 3, 7, 12), // Also rarer, was 16
+                new WeightedRandomChestContent(Items.gunpowder, 0, 3, 7, 5), // New
+                new WeightedRandomChestContent(Items.leather, 0, 3, 8, 5), // New
                 new WeightedRandomChestContent(Items.saddle, 0, 1, 1, 3),
                 new WeightedRandomChestContent(Items.iron_horse_armor, 0, 1, 1, 1),
                 new WeightedRandomChestContent(Items.golden_horse_armor, 0, 1, 1, 1),
