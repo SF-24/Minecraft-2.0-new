e494e246699cc56a49a1a7ebf45fbb99155f0084
SF-24 <legonexoknights123@gmail.com>
2026-07-13 12:30:26 +0200

    Added enchanted books to nether fortress loot. Made non structure enchanted books rarer. Increased level of min. enchanted books. Nether fortress chests can now contain any piece of golden armour.

--- src/main/java/net/mineshaft/structure/EnumLootSource.java
+++ src/main/java/net/mineshaft/structure/EnumLootSource.java
@@ -2,16 +2,18 @@ package net.mineshaft.structure;
 
 import net.minecraft.enchantment.Enchantment;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public enum EnumLootSource {
 
-    NETHER_FORTRESS(true, List.of(Enchantment.flame,Enchantment.fireAspect,Enchantment.fireProtection,Enchantment.smite)),
-    NETHER_BASEMENT(true, List.of()), // TODO:
-    JUNGLE_PYRAMID(false, List.of(Enchantment.baneOfArthropods,Enchantment.projectileProtection,Enchantment.punch)),
-    DESERT_PYRAMID(false, List.of(Enchantment.smite, Enchantment.blastProtection,Enchantment.knockback)),
-    STRONGHOLD(true, List.of(Enchantment.featherFalling, Enchantment.power, Enchantment.infinity)), // TODO:
-    DUNGEON(false, List.of()),;
+    NETHER_FORTRESS(true, Arrays.asList(Enchantment.flame, Enchantment.fireAspect, Enchantment.fireProtection, Enchantment.smite)),
+    NETHER_BASEMENT(true, Arrays.asList()), // TODO:
+    JUNGLE_PYRAMID(false, Arrays.asList(Enchantment.baneOfArthropods,Enchantment.projectileProtection,Enchantment.punch)),
+    DESERT_PYRAMID(false, Arrays.asList(Enchantment.smite, Enchantment.blastProtection,Enchantment.knockback)),
+    STRONGHOLD(true, Arrays.asList(Enchantment.featherFalling, Enchantment.power, Enchantment.infinity)), // TODO:
+    DUNGEON(false, Arrays.asList()),;
 
     private final List<Enchantment> nativeEnchantments;
     private final boolean allowSuperEnchants;
@@ -23,6 +25,10 @@ public enum EnumLootSource {
 
     public List<Enchantment> getNativeEnchantments() {
         return this.nativeEnchantments;
+    }
+
+    public Enchantment getRandomEnchantment(Random rand) {
+        return this.nativeEnchantments.get(rand.nextInt(this.nativeEnchantments.size()));
     }
 
     public boolean hasSuperEnchants() {
