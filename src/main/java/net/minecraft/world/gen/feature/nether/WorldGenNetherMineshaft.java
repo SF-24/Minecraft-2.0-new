f3c1af9de7701e83517f0ee646b1da149170c2e3
SF-24 <legonexoknights123@gmail.com>
2026-08-16 10:25:28 +0200

    Updated ore drop amounts to nerf fortune

---
+++ src/main/java/net/minecraft/world/gen/feature/nether/WorldGenNetherMineshaft.java
@@ -0,0 +1,22 @@
+package net.minecraft.world.gen.feature.nether;
+
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenerator;
+import net.mineshaft.structure.LootTableList;
+import net.mineshaft.structure.gen.TowerGen;
+
+import java.util.Random;
+
+public class WorldGenNetherMineshaft extends WorldGenerator {
+    @Override
+    public boolean generate(World worldIn, Random rand, BlockPos position) {
+        // Generate the tower
+        int yPosition = position.getY();
+        // TODO:!!!!=
+
+        return false;
+    }
+
+
+}
